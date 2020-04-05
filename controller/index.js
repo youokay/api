e7261eef2febcb84dbb5b3f84721f3f8d6ffe564
Emily Kwak <emilykwak1@gmail.com>
2020-04-05 09:43:22 -0700

    get all unique numbers from groups

--- controller/index.js
+++ controller/index.js
@@ -1,23 +1,30 @@
 const db = require('../db/index');
 const { User } = require('../db/index');
+var _ = require('lodash');
 
 // curl -d "@data.json" -H "Content-Type: application/json" -X POST http://localhost:3000/seed
 
 module.exports = {
-  checkIn (req, res, next) {
+  checkIn(req, res, next) {
     console.log('checked in');
     res.send('<h2>Thanks for checking in!</h2>');
   },
-  seedUser (req, res, next) {
+  seedUser(req, res, next) {
     const data = req.body;
     const number = data.phone;
     const user = new User(data);
-    db.User.updateOne({phone: number}, user, {upsert: true})
+    db.User.updateOne({ phone: number }, user, { upsert: true })
       .then(result => {
         res.status(201).send(result);
       })
       .catch(err => {
         res.status(404).send(err);
       });
+  },
+  numbers(req, res, next) {
+    const number = req.params.phone
+    db.User.findOne({ phone: number })
+      .then((result) => res.status(200).send(_.uniq(result.groups.map(group => group.phoneNumbers).flat())))
+      .catch(err => res.status(400).json(`Error: ${err}`));
   }
 }
