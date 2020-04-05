2b63268ac8e3f951a92db25b5bc86d561dd4124e
Manuela Gomez <mmanu.gomez@gmail.com>
2020-04-05 10:02:40 -0400

    Added environment variables to configure prod/dev database

---
+++ env.config.js
@@ -0,0 +1,16 @@
+// in development
+let obj = {
+  DATABASE_URL: `mongodb://localhost`,
+  DATABASE_NAME: 'youokay',
+  SERVICE_URL: `http://localhost`,
+  SERVICE_PORT: `3000`,
+};
+// in production
+if (process.env.NODE_ENV === 'production') {
+  obj = Object.assign({}, obj, {
+    DATABASE_URL: 'mongodb://database',
+    SERVICE_URL: process.env.SERVICE_CHART_URL
+  });
+}
+
+module.exports = obj;
