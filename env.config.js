6ddb2b97191c08a33f4cbe2f71ed468f87f3f76d
Manuela Gomez <mmanu.gomez@gmail.com>
2020-04-05 12:31:19 -0400

    Using previous port sytnax

--- env.config.js
+++ env.config.js
@@ -3,14 +3,13 @@ let obj = {
   DATABASE_URL: `mongodb://localhost`,
   DATABASE_NAME: 'youokay',
   SERVICE_URL: `http://localhost`,
-  SERVICE_PORT: `3000`,
+  SERVICE_PORT: `4444`,
 };
 // in production
 if (process.env.NODE_ENV === 'production') {
   obj = Object.assign({}, obj, {
     DATABASE_URL: 'mongodb://database',
-    SERVICE_URL: process.env.SERVICE_URL,
-    SERVICE_PORT: `4444`
+    SERVICE_URL: process.env.SERVICE_URL
   });
 }
 
